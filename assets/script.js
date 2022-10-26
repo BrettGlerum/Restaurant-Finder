b950bc9bb8f41b838cbcb025b13de6dc6f721df1
Nevin Lewis <64855834+Nevin-Lewis@users.noreply.github.com>
2022-10-25 20:01:39 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var state = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
-var city = []
+var city = [];
+var final_selection = [];
 
 const options = {
 	method: 'GET',
@@ -8,7 +9,12 @@ const options = {
 		'X-RapidAPI-Host': 'restaurants-near-me-usa.p.rapidapi.com'
 	}
 };
-fetch('https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/state/placeholder/city/placeholder/1', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+    fetch('https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/state/' + state + '/city/' + city +'/'+ number +', options')
+        .then(function (response) {
+            response.json().then(function (data){
+                localStorage.setItem("restaurants", JSON.stringify(data));
+               restaurant_options = JSON.parse(localStorage.getItem("restaurants"));
+               final_selection = restaurant_options.restaurants
+                console.log(final_selection)
+            })
+        })
