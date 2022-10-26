53fa3cf7a9de9446f79c516121d540a9ddddf703
Nevin Lewis <lewisnevin@utexas.edu>
2022-10-25 21:14:12 -0500

    added variables

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 var state = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 var city = ['Austin'];
 var final_selection = [];
+var names = [];
+var hours = [];
 
 const options = {
 	method: 'GET',
@@ -12,9 +14,13 @@ const options = {
     fetch('https://restaurants-near-me-usa.p.rapidapi.com/restaurants/location/state/' + state[42] + '/city/' + city[0] +'/3', options)
         .then(function (response) {
             response.json().then(function (data){
-                localStorage.setItem("restaurants", JSON.stringify(data));
-               restaurant_options = JSON.parse(localStorage.getItem("restaurants"));
-               final_selection = restaurant_options.restaurants
-                console.log(final_selection)
-            })
+                localStorage.setItem("restaurants", JSON.stringify(data));})
+               
         }); 
+restaurant_options = JSON.parse(localStorage.getItem("restaurants"));
+final_selection = restaurant_options.restaurants
+console.log(final_selection)
+    
+
+names = final_selection[0].restaurantName;
+hours = final_selection[0].intervalHours;
