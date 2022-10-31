356344f56e7c62d856aaf2a8baf5990694b70450
Nevin Lewis <lewisnevin@utexas.edu>
2022-10-31 18:02:37 -0500

    added directions to resturant

--- assets/google.js
+++ assets/google.js
@@ -32,16 +32,16 @@ if (document.title === 'Restaurant Finder') {
   if (document.title === 'Restaurant Finder Results') {
     
   var restultsEl = document.querySelectorAll(".result-item");
+  var restultsLocationEl = document.querySelectorAll(".result-loc")
   var map;
   var service;
   var infowindow;
+  var cost = localStorage.getItem("cost");
+var distance =  1609 * localStorage.getItem("distance");
+var locations = JSON.parse(localStorage.getItem("search_location"))[0].geometry.location;
 
   function initMap() {
-    var cost = localStorage.getItem("cost");
-    var distance =  1609 * localStorage.getItem("distance");
-    var locations = JSON.parse(localStorage.getItem("search_location"))[0].geometry.location;
-      infowindow = new google.maps.InfoWindow();
-    
+    infowindow = new google.maps.InfoWindow();
      map = new google.maps.Map(
           document.getElementById('map'), {center: locations, zoom: 15});
     
@@ -65,21 +65,89 @@ function callback(results, status)  {
         let sortedProducts = Results.sort(
             (p1, p2) => (p1.rating < p2.rating) ? 1 : (p1.rating > p2.rating) ? -1 : 0);
         console.log(sortedProducts);
-        for (var i = 0; i < Results.length; i++) {
+        for (var i = 0; i < 3; i++) {
         var marker = new google.maps.Marker(
           {place: {
-            placeId: results[i].place_id,
-            location: results[i].geometry.location
+            placeId: sortedProducts[i].place_id,
+            location: sortedProducts[i].geometry.location
           },
           map,
         title: results.title});
         }
         for (let i = 0; i < 3; i++){
           restultsEl[i].textContent = Results[i].name;
-        }
-        map.setCenter(results[0].geometry.location);
+          restultsLocationEl[i].textContent = Results[i].vicinity}
+        map.setCenter(sortedProducts[0].geometry.location);
       }
   
   };
+
+  function initMap2() {
+    var Results = JSON.parse(localStorage.getItem("results"));
+   const map= new google.maps.Map(
+        document.getElementById('map2'), {center: locations, zoom: 15});
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer({
+      draggable: true,
+      map,
+      panel: document.getElementById("panel"),
+    });
+  
+    directionsRenderer.addListener("directions_changed", () => {
+      const directions = directionsRenderer.getDirections();
+  
+      if (directions) {
+        computeTotalDistance(directions);
+      }
+    });
+    displayRoute(
+      locations,
+      Results[0].geometry.location,
+      directionsService,
+      directionsRenderer
+    );
+  }
+  
+  function displayRoute(origin, destination, service, display) {
+    service
+      .route({
+        origin: origin,
+        destination: destination,
+        travelMode: google.maps.TravelMode.DRIVING,
+        avoidTolls: true,
+      })
+      .then((result) => {
+        display.setDirections(result);
+      })
+      .catch((e) => {
+        alert("Could not display directions due to: " + e);
+      });
+  }
+  
+  function computeTotalDistance(result) {
+    let total = 0;
+    const myroute = result.routes[0];
+  
+    if (!myroute) {
+      return;
+    }
+  
+    for (let i = 0; i < myroute.legs.length; i++) {
+      total += myroute.legs[i].distance.value;
+    }
+  
+
+    document.getElementById("total").innerHTML = total;
+  };
+ var Search = document.querySelector(".results-list");
+
+  Search.addEventListener("click", function(event){
+    var element = event.target;
+    if(element.matches("button") === true){
+        initMap2();
+    }
+  });
   window.initMap = initMap;
+
+  
   };
